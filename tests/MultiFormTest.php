0512155cbeefe747875cceb400301ed5984ee932
Sean Harvey <sean@silverstripe.com>
2008-09-20 09:27:38 +0000

    MINOR Updated MultiFormTest class comments

--- tests/MultiFormTest.php
+++ tests/MultiFormTest.php
@@ -19,11 +19,6 @@
  * of MultiFormStep, which is every step in a form, requires a db/build, as it
  * is a subclass of DataObject. This is a bit of a pain, but it's required for
  * the database to store the step data for each step, which is very important!
- * 
- * @TODO make use of .yml file to populate test db data instead?
- * 
- * So, if you're going to create some new tests, and want to use some test classes,
- * make sure to use the ones mentioned above.
  */
 class MultiFormTest extends SapphireTest {
 
